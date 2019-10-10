0854b019fdd5a95c9595b4a0c21c2e760b7ecc7f
Yan Tavares G <yantavaresg@gmail.com>
2019-10-10 14:02:10 -0300

    estrutura base

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PrimeiroComponente from './componentes/PrimeiroComponente'
+
+const elemento = document.getElementById('root')
+ReactDOM.render(
+  <div>
+    <PrimeiroComponente></PrimeiroComponente>
+  </div>
+, elemento)
