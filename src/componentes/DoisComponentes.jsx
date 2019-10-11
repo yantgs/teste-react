6a6d4e800fe1c77752a9168898b2160efd87cceb
Yan Tavares G <yantavaresg@gmail.com>
2019-10-11 13:44:32 -0300

    testando props

--- src/componentes/DoisComponentes.jsx
+++ src/componentes/DoisComponentes.jsx
@@ -7,3 +7,5 @@ const CompB = props =>
 <h1>Segundo diz: {props.valor}</h1>
 
 export { CompA, CompB}
+
+
--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import PrimeiroComponente from './componentes/PrimeiroComponente'
-import { CompA, CompB } from './componentes/DoisComponentes'
-import MultiElementos from "./componentes/MultiElementos";
+// import { CompA, CompB } from './componentes/DoisComponentes'
+// import MultiElementos from "./componentes/MultiElementos";
 // import MultiElementos from './componentes/MultiElementos'
+// import FamiliaSilva from './componentes/FamiliaSilva'
+import Familia from './componentes/Familia'
+import Membro from './componentes/Membro'
+
 
 const elemento = document.getElementById("root");
 ReactDOM.render(
   <div>
+    <Familia  sobrenome="Pereira">
+      <Membro nome="André" />
+      {/* <Membro nome="Mariana" sobrenome="Pereira" /> */}
+    </Familia>
+    {/* <FamiliaSilva /> */}
     {/* <MultiElementos /> */}
-    <CompA valor="Comp A" />
-    <CompB valor="Comp B" />
+    {/* <CompA valor="Comp A" /> */}
+    {/* <CompB valor="Comp B" /> */}
     {/* <PrimeiroComponente /> */}
   </div>,
   elemento
